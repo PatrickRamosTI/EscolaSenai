ee37ef4c4241cd4b68577e42b69cc20ac1762187
PatrickRamosTI <patrickramosTi@gmail.com>
2024-09-14 09:59:00 -0300

    Update UnidadeFederal.java

--- src/main/java/com/poo/escola/entidades/UnidadeFederal.java
+++ src/main/java/com/poo/escola/entidades/UnidadeFederal.java
@@ -1,46 +1,50 @@
-package com.poo.escola.entidades;
+package com.poo.escola.enuns;
 
 public enum UnidadeFederal {
 
-   RJ("Rio de Janeiro"), 
-   SP("São Paulo"), 
-   MG("Minas Gerais"), 
-   ES("Espírito Santo"),
+    RJ("Rio de Janeiro"),
+    SP("São Paulo"),
+    MG("Minas Gerais"),
+    ES("Espírito Santo"),
 
-   PR("Paraná"),
-   SC("Santa Catarina"),
-   RS("Rio Grande do Sul"),
 
-   DF("Distrito Federal"),
-   GO("Goiás"),
-   MS("Mato Grosso do Sul"),
-   MT("Mato Grosso"),
+    PR("Paraná"),
+    SC("Santa Catarina"),
+    RS("Rio Grande do Sul"),
 
-   AL("Alagoas"),
-   BA("Bahia"),
-   CE("Ceará"),
-   MA("Maranhão"),
-   PB("Paraíba"),
-   PE("Pernambuco"),
-   PI("Piauí"),
-   RN("Rio Grande do Norte"),
-   SE("Sergipe"),
 
-   AC("Acre"),
-   AP("Amapá"),
-   AM("Amazonas"),
-   PA("Pará"),
-   RO("Rondônia"),
-   RR("Roraima"),
-   TO("Tocantins");
+    DF("Distrito Federal"),
+    GO("Goiás"),
+    MS("Mato Grosso do Sul"),
+    MT("Mato Grosso"),
 
-   private final String nomePorExtenso;
-   
-   UnidadeFederal(String nomePorExtenso) {
-       this.nomePorExtenso = nomePorExtenso;
-   }
 
-   public String getNomePorExtenso() {
-       return nomePorExtenso;
-   }
+    AL("Alagoas"),
+    BA("Bahia"),
+    CE("Ceará"),
+    MA("Maranhão"),
+    PB("Paraíba"),
+    PE("Pernambuco"),
+    PI("Piauí"),
+    RN("Rio Grande do Norte"),
+    SE("Sergipe"),
+
+
+    AC("Acre"),
+    AP("Amapá"),
+    AM("Amazonas"),
+    PA("Pará"),
+    RO("Rondônia"),
+    RR("Roraima"),
+    TO("Tocantins");
+
+    private final String nomePorExtenso;
+
+    UnidadeFederal(String nomePorExtenso){
+        this.nomePorExtenso = nomePorExtenso;
+    }
+
+    public String getNomePorExtenso() {
+        return nomePorExtenso;
+    }
 }
